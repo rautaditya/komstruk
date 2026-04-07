e2740b97965b97c13abffb888bf36196fd98b07d
AdityaRaut <adityaraut6029@gmail.com>
2026-04-07 16:54:12 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: 'automatic',
-      babel: {
-        presets: [],
-        plugins: [],
-      },
-    }),
-  ],
+  plugins: [react()]
 })
