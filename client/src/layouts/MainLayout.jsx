ee30846e27291301db48244597ceaf1f67bf981f
AdityaRaut <adityaraut6029@gmail.com>
2026-04-07 10:02:19 +0530

    fix jsx runtime conflict

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "../componants/Header";
 import Footer from "../componants/Footer";
 import { Outlet } from "react-router-dom";
--- client/src/pages/Application.jsx
+++ client/src/pages/Application.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "../componants/Header";
 import Footer from "../componants/Footer";
 import { useEffect, useRef, useState } from "react";
